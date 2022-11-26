8f625788b90e4d76c473ebefb9326cdc54fa752c
Arya Shreyas <aryashreyasdas@gmail.com>
2022-11-27 01:10:09 +0530

    some changes

--- achievements.js
+++ achievements.js
@@ -1,44 +1,44 @@
 export const achievements = [
   {
     id: 1,
-    title: "ECOKART",
+    title: "REVIT 2021",
     details:
       "AT BML Munjal University, DELHI, AIR-3 in Virtual Round nominated for best design and innovation, in 2015.",
-    date: "ECOKART 2015",
+    date: "October 2021",
   },
   {
-    id: 1,
-    title: "ECOKART",
+    id: 2,
+    title: "FMAE 2021",
     details:
       "AT BML Munjal University, DELHI, AIR-3 in Virtual Round nominated for best design and innovation, in 2015.",
-    date: "ECOKART 2015",
+    date: "September 2021",
   },
   {
-    id: 1,
-    title: "ECOKART",
+    id: 3,
+    title: "BAJA 2021",
     details:
       "AT BML Munjal University, DELHI, AIR-3 in Virtual Round nominated for best design and innovation, in 2015.",
-    date: "ECOKART 2015",
+    date: "April 2021",
   },
   {
-    id: 1,
-    title: "ECOKART",
+    id: 4,
+    title: "GO KART 2020",
     details:
       "AT BML Munjal University, DELHI, AIR-3 in Virtual Round nominated for best design and innovation, in 2015.",
-    date: "ECOKART 2015",
+    date: "January 2021",
   },
   {
-    id: 1,
-    title: "ECOKART",
+    id: 5,
+    title: "BAJA 2019",
     details:
       "AT BML Munjal University, DELHI, AIR-3 in Virtual Round nominated for best design and innovation, in 2015.",
-    date: "ECOKART 2015",
+    date: "March 2020",
   },
   {
-    id: 1,
-    title: "ECOKART",
+    id: 6,
+    title: "ESI 2017",
     details:
       "AT BML Munjal University, DELHI, AIR-3 in Virtual Round nominated for best design and innovation, in 2015.",
-    date: "ECOKART 2015",
+    date: "February 2017",
   },
 ];
