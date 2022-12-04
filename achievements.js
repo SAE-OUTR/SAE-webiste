cbefb359026497b7846fa93eea6c1a91ff060392
Arya Shreyas <aryashreyasdas@gmail.com>
2022-12-04 18:16:31 +0530

    added achievements

--- achievements.js
+++ achievements.js
@@ -1,44 +1,45 @@
 export const achievements = [
   {
     id: 1,
-    title: "REVIT 2021",
+    title: "ESI 2018",
     details:
-      "AT BML Munjal University, DELHI, AIR-3 in Virtual Round nominated for best design and innovation, in 2015.",
-    date: "October 2021",
+      "Enduro Student India is an offroad student design comptition. Secured AIR 51 & AIR 43 in ESI 2018 and AIR 2019 respectively.",
+    date: "February 2018",
   },
   {
     id: 2,
-    title: "FMAE 2021",
+    title: "ECO CART 2020",
     details:
-      "AT BML Munjal University, DELHI, AIR-3 in Virtual Round nominated for best design and innovation, in 2015.",
-    date: "September 2021",
-  },
-  {
-    id: 3,
-    title: "BAJA 2021",
-    details:
-      "AT BML Munjal University, DELHI, AIR-3 in Virtual Round nominated for best design and innovation, in 2015.",
-    date: "April 2021",
-  },
-  {
-    id: 4,
-    title: "GO KART 2020",
-    details:
-      "AT BML Munjal University, DELHI, AIR-3 in Virtual Round nominated for best design and innovation, in 2015.",
-    date: "January 2021",
-  },
-  {
-    id: 5,
-    title: "BAJA 2019",
-    details:
-      "AT BML Munjal University, DELHI, AIR-3 in Virtual Round nominated for best design and innovation, in 2015.",
+      "Debut year of Eco-Kart received AIR 4 in virtual along with design and inovation award.",
     date: "March 2020",
   },
   {
-    id: 6,
-    title: "ESI 2017",
+    id: 3,
+    title: "GO KART 2020",
     details:
-      "AT BML Munjal University, DELHI, AIR-3 in Virtual Round nominated for best design and innovation, in 2015.",
-    date: "February 2017",
+      "Participated in International Go-Kart Championship & team was runnersip overalland first place in Business Presentation.",
+    date: "January 2021",
+  },
+
+  {
+    id: 4,
+    title: "SAE BAJA 2021",
+    details:
+      "Qualified for e-BAJA and m-BAJA virtually & secured a rank of AIR 17 in e-BAJA virtuals. Qualified fot the dynamic event in BAJA 2019, 2020, 2021(virtual)",
+    date: "April 2021",
+  },
+  {
+    id: 5,
+    title: "FMAE 2021",
+    details:
+      "National Online Electic Bike Design Competition. Secured AIR 6 in FMAE Online e-Bike Design. Secured AIR 6 in online e-Bike design.",
+    date: "September 2021",
+  },
+  {
+    id: 6,
+    title: "REVIT 2021",
+    details:
+      "Oraganized by Formual Bharat t& IR eSports. Secured an overall rank of AIR 7 among prominent Indian teams from IITs & NITs",
+    date: "October 2021",
   },
 ];
