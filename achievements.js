eb70f01d29d384c5eeb97a9e136a9bdf1b5c2007
Arya Shreyas <aryashreyasdas@gmail.com>
2023-01-02 17:47:48 +0530

    corrections

--- achievements.js
+++ achievements.js
@@ -3,7 +3,7 @@ export const achievements = [
     id: 1,
     title: "ESI 2018",
     details:
-      "Enduro Student India is an offroad student design comptition. Secured AIR 51 & AIR 43 in ESI 2018 and AIR 2019 respectively.",
+      "Enduro Student India is an offroad student design comptition. Secured AIR 51 & AIR 43 in ESI 2018 and 2019 respectively.",
     date: "February 2018",
   },
   {
@@ -25,21 +25,21 @@ export const achievements = [
     id: 4,
     title: "SAE BAJA 2021",
     details:
-      "Qualified for e-BAJA and m-BAJA virtually & secured a rank of AIR 17 in e-BAJA virtuals. Qualified fot the dynamic event in BAJA 2019, 2020, 2021(virtual).",
+      "Qualified for e-BAJA and m-BAJA virtually & secured a rank of AIR 17 in e-BAJA virtuals. Qualified for the dynamic event in BAJA 2019, 2020, 2021(virtual).",
     date: "April 2021",
   },
   {
     id: 5,
     title: "FMAE 2021",
     details:
-      "National Online Electic Bike Design Competition. Secured AIR 6 in FMAE Online e-Bike Design. Secured AIR 6 in online e-Bike design.",
+      "National Online Electic Bike Design Competition. Secured AIR 6 in FMAE Online e-Bike Design",
     date: "September 2021",
   },
   {
     id: 6,
     title: "REVIT 2021",
     details:
-      "Oraganized by Formual Bharat t& IR eSports. Secured an overall rank of AIR 7 among prominent Indian teams from IITs & NITs",
+      "Oraganized by Formula Bharat & IR eSports. Secured an overall rank of AIR 7 among prominent Indian teams from IITs & NITs",
     date: "October 2021",
   },
 ];
